e6d1ee1c0157e2567aa9c58ae958f46147b51e43
Woshiajuana <zhigang.chen@owulia.com>
2020-11-22 20:05:30 +0800

    subcode

--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -74,4 +74,19 @@ module.exports = {
     // 模式
     mode: 'development',
     // mode: 'production',
+
+    // devServer
+    // 自动编译，自动打开浏览器，字段刷新浏览器
+    // 特点：只会在内存中编译打包，不会有任何输出
+    // 启动 devServer 指令为：webpack-dev-server
+    devServer: {
+        // 运行的目录
+        contentBase: path.resolve(__dirname, 'dist'),
+        // 启动 gzip 压缩
+        compress: true,
+        // 服务端口
+        port: 3000,
+        // 自动打开浏览器
+        open: true,
+    },
 };
